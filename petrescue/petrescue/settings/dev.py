1d94e8c5c8a29a441527156682148e780a414146
Jason Judkins <34417573+jcjudkins@users.noreply.github.com>
2022-01-19 10:46:10 -0500

    install files for wagtail

---
+++ petrescue/petrescue/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-8jo+ji12v^5$jrab&4d@rn+vu*j^gh22xl$c=0j_%%igz_x1r('
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
