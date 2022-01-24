33f959b90c45b48e30144f2272da4b5e61c6e459
janelle-bouchard <91432686+janelle-bouchard@users.noreply.github.com>
2022-01-24 16:55:26 -0500

    messed something up in the way of the views.py, so I replaced the file with exactly what was on remote main and I'm not crying out of frustration or anything

--- petrescue/views.py
+++ petrescue/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
-from .serializers import FosterSerializer, PetSerializer
+from .serializers import FosterSerializer, PetSerializer, AgencySerializer, ApplicantSerializer
 from rest_framework import generics
 from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView, RetrieveDestroyAPIView
-from .models import Foster, Pet, Agency
+from .models import Foster, Pet, Applicant, Agency
+from rest_framework import viewsets
+from rest_framework.viewsets import ModelViewSet
 
 
 
 # Create your views here.
 def homepage(request):
-    # pets=["Graham", "Trixie", "Gigi", "Kingsley"]
-    # return render(request, "petrescue/homepage.html", {"pets": pets})
-    pets = Pet.objects.all()
-    return render(request, "petrescue/homepage.html", {
-        "pets": pets })
+    pets=["Graham", "Trixie", "Gigi", "Kingsley"]
+    return render(request, "petrescue/homepage.html", {"pets": pets})
+
 
 class FosterList(generics.ListCreateAPIView):
     queryset = Foster.objects.all()
@@ -61,3 +61,29 @@ class DeletePet(RetrieveDestroyAPIView):
 
 class NewPet(CreateAPIView):
     serializer_class = PetSerializer
+
+
+class ApplicantViewSet(ModelViewSet):
+    queryset = Applicant.objects.all()
+    serializer_class = ApplicantSerializer
+    permission_classes = []
+
+    def get_serializer_class(self):
+        if self.action in ["list"]:
+          return ApplicantSerializer
+        return super().get_serializer_class()
+
+
+class AgencyViewSet(ModelViewSet):
+    queryset = Agency.objects.all()
+    serializer_class = AgencySerializer
+    permission_classes = []
+
+    def get_serializer_class(self):
+        if self.action in ["list"]:
+           return AgencySerializer
+        return super().get_serializer_class()
+
+
+
+
