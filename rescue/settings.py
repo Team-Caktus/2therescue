ea686e3eeda27c1d21dc033bdebe6fb04cd18984
Keanya Phelps <keanyaphelps@gmail.com>
2022-01-25 08:01:09 -0600

    commented out secretkey

--- rescue/settings.py
+++ rescue/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('86eq(64mq$5$2xuzq+2$e=*i2jjjqs)pkcbxrqni5965j7@joy')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
