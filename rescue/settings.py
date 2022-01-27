55be53aa9de8a71e39011f5e96ccaf0afcae4ca1
janelle-bouchard <91432686+janelle-bouchard@users.noreply.github.com>
2022-01-27 16:10:12 -0500

    Updated MEDIA_URL and some AWS and other Media settings based on what the AWS implementation needs to function. Also updated the image template tags and successfully able to pull the correct url from AWS to our pages now, but still having the security issue with AWS so we aren't able to see them render on the page yet.

--- rescue/settings.py
+++ rescue/settings.py
@@ -158,8 +158,9 @@ AUTH_USER_MODEL = 'petrescue.CustomUser'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+# MEDIA_DIR = BASE_DIR / 'media'
 
 WAGTAIL_SITE_NAME = '2TheRescue'
 
@@ -182,10 +183,12 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-AWS_LOCATION = 'media'
+# AWS_LOCATION = 'media'
 
-MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-# MEDIAFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_FILE_OVERWRITE = False 
+AWS_DEFAULT_ACL = 'public-read'
 
-DEFAULT_FILE_STORAGE = 'rescue.storage_backends.MediaStorage'  
+# 'rescue.storage_backends.MediaStorage'  
 # MEDIA_URL = 'https://teamcaktuspetphotos.s3.amazonaws.com/media/'
