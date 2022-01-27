52e4c4204abe338865cab976d23d7c4a08fdf1ea
Jason Judkins <34417573+jcjudkins@users.noreply.github.com>
2022-01-27 09:55:16 -0500

    implemented AWS

--- rescue/settings.py
+++ rescue/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
     'phonenumber_field',
     'wagtail.contrib.modeladmin',
     'rest_framework',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -171,3 +172,22 @@ WAGTAILSEARCH_BACKENDS = {
 # this code causes error, but was on install list for heroku
 django_on_heroku.settings(locals()) 
 del DATABASES['default']['OPTIONS']['sslmode']
+
+
+
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'teamcaktuspetphotos'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+DEFAULT_FILE_STORAGE = 'rescue.storage_backends.MediaStorage'  
